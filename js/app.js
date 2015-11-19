706b5dd7c4025d2a8bd226a9774331a2b4578ec5
cindy <cnguyen0@uw.edu>
2015-11-19 15:26:53 -0800

    gottttemmmmm

--- js/app.js
+++ js/app.js
@@ -8,5 +8,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     'use strict';
 
+    var canvas = document.getElementById('drawCanvas');
+    var ctx = canvas.getContext('2d');
 
+
+    var truth = false;
+
+    document.addEventListener('mousedown', function(evt) {
+        var canvasY = evt.clientY - canvas.offsetTop;
+        //properties evt.clientX evt.clientY
+
+        ctx.beginPath();
+        ctx.moveTo(evt.clientX,canvasY);
+        ctx.stroke();
+        truth = true;
+
+        document.addEventListener('mouseup', function(evt) {
+            truth = false;
+        });
+
+        document.addEventListener('mousemove', function(evt) {
+            var canvasY = evt.clientY - canvas.offsetTop;
+            if (truth) {
+                ctx.strokeStyle = (document.getElementById('line-color-inp')).value;
+                ctx.lineTo(evt.clientX,canvasY);
+                ctx.stroke();
+            }
+        });
+    });
 }); //DOMContentLoaded
